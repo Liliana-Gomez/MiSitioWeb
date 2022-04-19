df10e62ebefff4e84582384a89c67f16f5f7391f
liliana <gicelagomez17082020@gmail.com>
2022-04-19 12:06:29 -0500

    todo

--- js/vuelos.js
+++ js/vuelos.js
@@ -1,77 +1,77 @@
-"user strict"
-
-let horaSalida = new Date(2022, 4, 5, 7, 30, 0);
-let horaLlegada = new Date(2022, 4, 5, 11, 30, 0);
-
-let horaSalida2 = new Date(2022, 4, 5, 9, 10, 0);
-let horaLlegada2 = new Date(2022, 4, 5, 1, 10, 0);
-
+"use strict"
 class Vuelos {
-    constructor (origen, destino, precio,) {
-        this.Origen = origen ;
-        this.Destino = destino;
-        this.Precio = precio;
-        this.horaSalida = horaSalida;
-        this.horaLlegada = horaLlegada;
+    constructor (origen, destino, precio) {
+        this.origen = origen ;
+        this.destino = destino;
+        this.precio = precio;
+        this.horaInicio = horaInicio;
+        this.horaFinal = horaFinal;
     }
 }
+
 let vuelo = [{
-    "origen" : "BOGOTA",
-    "destino" : "CARTAGENA",
-    "precio" : 30000,
-    "horaSalida" : horaSalida.getHours() + horaSalida.getMinutes(), 
-    "horaLlegada" : horaLlegada.getHours() + horaLlegada.getMinutes(), 
+    "origen": "Bogota",
+    "destino": "Barranquilla",
+    "precio": 800000,
+    "horaInicio": "4:00AM",
+    "horaFinal": "10:00PM",
 },
-  {
-    "origen" : "CALI",
-    "destino" : "BOGOTA",
-    "precio" : 50000,
-    "horaSalida" : horaSalida2.getHours() + horaSalida2.getMinutes(), 
-    "horaLlegada" : horaLlegada2.getHours() + horaLlegada2.getMinutes(), 
+{
+    "origen": "Bogota",
+    "destino": "Cauca",
+    "precio": 780000,
+    "horaInicio": "6:00AM",
+    "horaFinal": "10:00AM",
 }];
 
 function mostrarVuelos() {
     for (let i = 0; i < vuelo.length; i++) {
-        cargarVuelos(vuelo[i].origen,  vuelo[i].destino, vuelo[i].precio, vuelo[i].horaSalida, vuelo[i].horaLlegada );
-    }
+    cargarVuelos(vuelo[i].origen, vuelo[i].destino, vuelo[i].precio, vuelo[i].horaInicio, vuelo[i].horaFinal);
 }
-function cargarVuelos (origen,destino,precio,horaSalida,horaLlegada) {
-    let contentVuelo = document.createElement("div");
-    let contentPrincipal = document.getElementById("main_content");
-    contentPrincipal.appendChild(contentVuelo);
-    contentVuelo.setAttribute("class","style-contentVuelo")
-
-    let contentOrigen = document.createElement("div");
-     contentVuelo.appendChild(contentOrigen);
-
-     let origenVuelo = document.createElement("label");
-    contentVuelo.appendChild(origenVuelo);
-    let textOrigen = document.createTextNode(origen);
-    origenVuelo.appendChild(textOrigen);
-    origenVuelo.setAttribute ("class","styleOrigen")
-
-    let destinoVuelo = document.createElement("label");
-    contentVuelo.appendChild(destinoVuelo);
-    let textDestino = document.createTextNode(destino);
-    destinoVuelo.appendChild(textDestino);
-    destinoVuelo.setAttribute ("class","styleDestino")
-
-    let precioVuelo = document.createElement("label");
-    contentVuelo.appendChild(precioVuelo);
-    let textPrecio = document.createTextNode(precio);
-    precioVuelo.appendChild(textPrecio);
-    precioVuelo.setAttribute ("class","stylePrecio")
-
-    let horaSalidaVuelo = document.createElement("label");
-    contentVuelo.appendChild(horaSalidaVuelo);
-    let textHoraSalida = document.createTextNode(horaSalida);
-    horaSalidaVuelo.appendChild(textHoraSalida);
-    //origenVuelo.setAttribute ("class","styleHoraSalida")
-
-    let horaLlegadaVuelo = document.createElement("label");
-    contentVuelo.appendChild(horaLlegadaVuelo);
-    let textHoraLlegada = document.createTextNode(horaLlegada);
-    horaLlegadaVuelo.appendChild(textHoraLlegada);
-   // origenVuelo.setAttribute ("class","styleHoraLlegada")
 }
 
+function cargarVuelos(origen,destino, precio){
+  let contentVuelo = document.createElement("div");
+  let contentPrincipal = document.getElementById("main_content");
+  contentPrincipal.appendChild(contentVuelo);
+  contentVuelo.setAttribute("class", "style-contentVuelo");
+
+  let contentOrigen = document.createElement("div");
+  contentVuelo.appendChild(contentOrigen);
+
+  let origenVuelo = document.createElement("label");
+  contentVuelo.appendChild(origenVuelo);
+  let textOrigen = document.createTextNode(origen);
+  origenVuelo.appendChild(textOrigen);
+  origenVuelo.setAttribute("class", "styleOrigen");
+
+  let destinoVuelo = document.createElement("label");
+  contentVuelo.appendChild(destinoVuelo);
+  let textDestino = document.createTextNode(destino);
+  destinoVuelo.appendChild(textDestino);
+  destinoVuelo.setAttribute("class", "styleDestino");
+
+  let precioVuelo = document.createElement("label");
+  contentVuelo.appendChild(precioVuelo);
+  let textPrecio = document.createTextNode(precio);
+  precioVuelo.appendChild(textPrecio);
+  precioVuelo.setAttribute("class", "stylePrecio");
+
+  let directVuelo = document.createElement("label");
+  contentVuelo.appendChild(directVuelo);
+  let textDirect = document.createTextNode("Directo");
+  directVuelo.appendChild(textDirect);
+  directVuelo.setAttribute("class", "styleDirecto");
+
+  let horaInicioVuelo = document.createElement("label");
+  contentVuelo.appendChild(horaInicioVuelo);
+  let textHoraInicio = document.createTextNode("10:00AM");
+  horaInicioVuelo.appendChild(textHoraInicio);
+  horaInicioVuelo.setAttribute("class", "styleHoraInicio");
+
+  let horaFinalVuelo = document.createElement("label");
+  contentVuelo.appendChild(horaFinalVuelo);
+  let textHoraFinal = document.createTextNode("4:00PM");
+  horaFinalVuelo.appendChild(textHoraFinal);
+  horaFinalVuelo.setAttribute("class", "styleHoraFinal");
+}
